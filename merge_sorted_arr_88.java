bb0a7c18aa0b9999c6993d14cce775066a5207ca
SAMUELKUMAR2 <146331228+SAMUELKUMAR2@users.noreply.github.com>
2023-11-23 00:29:33 +0530

    88.merge sorted array

---
+++ merge_sorted_arr_88.java
@@ -0,0 +1,19 @@
+
+public class merge_sorted_arr_88 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int []nums1 = {1,2,3}; 
+		  int m = 3;
+		 int []nums2 = {2,5,6};
+			int	 n = 3;
+			nums1= new int[m+n];
+			for(int i = 0;i<m;i++) {
+				nums1[i]=nums1[i];
+			}
+			for(int i=0;i<n;i++) {
+				nums1[i]=nums2[i];
+			}
+	}
+
+}
